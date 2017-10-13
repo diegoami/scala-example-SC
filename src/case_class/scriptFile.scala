f0f938199d1c1377c8495be8acfe2343d626fad2
Diego Amicabile <diego.amicabile@gmail.com>
2017-10-13 17:19:39 +0200

    Stuff scala

---
+++ src/case_class/scriptFile.scala
@@ -0,0 +1,7 @@
+sealed abstract class Furniture
+case class Couch() extends Furniture
+case class Chair() extends Furniture
+def findPlaceToSit(piece: Furniture): String = piece match {
+  case a: Couch => "Lie on the couch"
+  case b: Chair => "Sit on the chair"
+}
